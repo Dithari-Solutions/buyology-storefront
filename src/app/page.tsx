815dcbfb790ffd5a8ca59da9b459e45dc5b6a55b
Hazira Hidayatova <hazirahidayatova@Hazira-MacBook-Air.local>
2026-02-12 16:54:03 +0400

    feat: slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 
 import SocialButtons from "@/features/auth/components/SocialButtons";
+import Stories from "@/features/home/components/Stories";
+import Header from "@/shared/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      <Header />
+      <main>
+        <Stories/>
+      </main>
     </>
   );
 }
